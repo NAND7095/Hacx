ea89b7f5f171b35aa54b8084694e78c637588bf4
Champu <112340896+ChampuXD@users.noreply.github.com>
2024-06-05 23:07:45 +0530

    fix

--- Hack/plugins/start.py
+++ Hack/plugins/start.py
@@ -1,12 +1,12 @@
 import env
-from Hack import bot
+from Hack import bot, app
 from Hack.helpers import MENU1, KEYBOARD1, PM_TEXT, PM_BUTTON
 from Hack.database import DB
 from pyrogram import filters
 from pyrogram.types import CallbackQuery
 
 
-@bot.on_message(filters.command("start") & filters.private)
+@app.on_message(filters.command("start") & filters.private)
 async def _start(_, message):
     user_id = message.from_user.id
     user = message.from_user.mention
@@ -22,13 +22,13 @@ async def _start(_, message):
        reply_markup = PM_BUTTON) 
 
 
-@bot.on_message(filters.command("hack") & filters.private)
+@app.on_message(filters.command("hack") & filters.private)
 async def _hack(_, message):
     await message.reply_text(MENU1,
               reply_markup = KEYBOARD1) 
 
 
-@bot.on_callback_query(filters.regex("hack_btn"))
+@app.on_callback_query(filters.regex("hack_btn"))
 async def heck_callback(bot : app, query : CallbackQuery):
     await query.message.delete()
     await query.message.reply_text(MENU1,
